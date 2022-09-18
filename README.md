204682a1d8f7ddcf2d4a71f04d53e7b9ec22bd07
NebiatH Nana <nebiathn@gmail.com>
2022-09-18 15:59:20 -0400

    README file updated

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Password Generator Starter Code
+
+# Password Generator 
+
+## About
+
+Based on a starter code application is created for an employee that can to used to generate a random password based on criteria they’ve selected.
+
+## Done with 
+
+**HTML, CSS & JS** 
+
+## Website 
+
+
+
+
+## Contribution
+
+NebiatHNana
--- script.js
+++ script.js
@@ -1,16 +1,71 @@
 // Assignment code here
+var UppCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ['"', "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var prePassword = [];
+var finalPassword = [];
 
+
+var combine = function (confirm, combine) {
+    if (confirm) {
+        prePassword = prePassword.concat(combine);
+    }
+}
+
+var PassWordGenerator = function () {
+
+
+    var passLenght = prompt("How long do you want the password to be? The minimum is 8, Please pick a number between 8 and 128");
+    if (!passLenght || passLenght < 8 || passLenght > 128) {
+        window.alert("Please pick a number between 8 and 128!");
+        return PassWordGenerator();
+    }
+
+
+    var upperConf = confirm("Do you want add uppercase letters to your password?");
+    var lowerConf= confirm("Do you want to add lowerCase letters to your password?");
+    var numberConf = confirm("Do you want to add numbers to your password?");
+    var specialConf = confirm("Do you like to add special characters to your password?");
+
+
+    if (!(upperConf || lowerConf|| numberConf || specialConf)) {
+        window.alert("You must pick one of the options.");
+        return PassWordGenerator();
+    };
+
+    combine(upperConf, UppCase);
+    combine(lowerConf, lowCase);
+    combine(numberConf, number);
+    combine(specialConf, special);
+
+
+    while (finalPassword.length < passLenght) {
+        random = prePassword[Math.floor(Math.random() * prePassword.length)];
+        finalPassword = finalPassword + random;
+    };
+}
+
+
+var reset = function () {
+    finalPassword = []
+}
+
+console.log(finalPassword);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  PassWordGenerator();
+
+  var password = finalPassword;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  reset();
 }
 
 // Add event listener to generate button
